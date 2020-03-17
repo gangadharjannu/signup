11811db2f8f4eb60933bbecad078b5028d2d4829
gangadhara.jannu <gangadhara.jannu@eurail.com>
2020-03-17 23:36:59 +0100

    Feature: Added e2e test case

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -13,6 +13,12 @@ describe('workspace-project App', () => {
     expect(page.getTitleText()).toEqual('Please register here to create account');
   });
 
+  it('should submit signup form', () => {
+    page.navigateTo();
+    page.setFormValues();
+    expect(page.getNotificationMessage()).toEqual('Registration successful');
+  });
+
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
