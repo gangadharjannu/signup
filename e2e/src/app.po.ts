aa43d894daa602fddcb808a57e8bc4009f4449d9
gangadhara.jannu <gangadhara.jannu@eurail.com>
2020-03-17 19:16:06 +0100

    Feature: Fixed e2e test cases

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+    return element(by.css('app-root .toolbar .title')).getText() as Promise<string>;
   }
 }
