7f59d875d0ced43721e0b3b9c00ab755dd7c9c8b
gangadharjannu <gangadhar014@gmail.com>
2020-07-02 22:15:00 +0200

    Feature - Updates signup component with constants

--- src/app/registration/signup/signup.component.ts
+++ src/app/registration/signup/signup.component.ts
@@ -1,13 +1,12 @@
-import { User } from './../../models/user';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { first } from 'rxjs/operators';
 import { MatSnackBar } from '@angular/material/snack-bar';
-
+import { first } from 'rxjs/operators';
+import { User } from './../../models/user';
 import * as signupFormConstants from './../registration.constants';
+import { RegistrationService } from './../registration.service';
 // import custom validator to validate that password and confirm password fields match
 import { ForbiddenPassword } from './forbidden-password.validator';
-import { RegistrationService } from './../registration.service';
 
 @Component({
   selector: 'app-signup',
@@ -22,12 +21,14 @@ export class SignupComponent implements OnInit {
   constructor(private fb: FormBuilder, private snackBar: MatSnackBar, private registerService: RegistrationService) { }
 
   ngOnInit(): void {
+    // initialize signup form with validations using form builder
     this.signupForm = this.fb.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(signupFormConstants.PASSWORD_MIN_LENGTH)]]
     }, {
+      // ForbiddenPassword accepts the property keys or restricted passwords
       validator: ForbiddenPassword('password', 'firstName', 'lastName')
     });
   }
@@ -35,59 +36,68 @@ export class SignupComponent implements OnInit {
   // convenience getter for easy access to form fields
   get f() { return this.signupForm.controls; }
 
+  /**
+   * method to execute when user submits the form
+   * registers the user using register service only if the form is valid
+   */
   submitSignupForm(): void {
     this.submitted = true;
-    // stop here if form is invalid
+    // Don't register the user if the form is invalid
     if (this.signupForm.invalid) {
       return;
     }
     const payload: User = { ...this.signupForm.value };
     delete payload.password;
 
+    // register the user
     this.registerService.register(payload).pipe(first())
       .subscribe(
         data => {
-          this.snackBar.open('Registration successful', 'Close', { duration: 3000 });
+          this.snackBar.open(
+            signupFormConstants.USER_REGISTRATION_SUCCESSFUL,
+            signupFormConstants.USER_REGISTRATION_ALERT_CLOSE,
+            { duration: signupFormConstants.SUCCESSFUL_MSG_DURATION }
+          );
         },
         error => {
-          this.snackBar.open('Registration failed', 'Close', { duration: 5000 });
+          this.snackBar.open(
+            signupFormConstants.USER_REGISTRATION_FAIL,
+            signupFormConstants.USER_REGISTRATION_ALERT_CLOSE,
+            { duration: signupFormConstants.FAILURE_MSG_DURATION }
+          );
         });
   }
 
-
+  // helper methods to get error messages for form fields
   getFirstNameErrorMessage() {
     if (this.f.firstName.hasError('required')) {
-      return 'You must enter a value';
+      return signupFormConstants.REQUIRED_VALIDATION_MSG;
     }
-
-    return this.f.firstName.hasError('firstName') ? 'Not a valid firstName' : '';
   }
 
   getLastNameErrorMessage() {
     if (this.f.lastName.hasError('required')) {
-      return 'You must enter a value';
+      return signupFormConstants.REQUIRED_VALIDATION_MSG;
     }
-
-    return this.f.lastName.hasError('lastName') ? 'Not a valid lastName' : '';
   }
 
   getEmailErrorMessage() {
     if (this.f.email.hasError('required')) {
-      return 'You must enter a value';
+      return signupFormConstants.REQUIRED_VALIDATION_MSG;
     }
 
-    return this.f.email.hasError('email') ? 'Not a valid email' : '';
+    return this.f.email.hasError('email') ? signupFormConstants.INVALID_EMAIL_MSG : '';
   }
+
   getPasswordErrorMessage() {
     if (this.f.password.hasError('required')) {
-      return 'You must enter a value';
+      return signupFormConstants.REQUIRED_VALIDATION_MSG;
     }
     if (this.f.password.hasError('minlength')) {
-      return 'Password should be at least 8 characters length';
+      return signupFormConstants.PASSWORD_MIN_LENGTH_VALIDATION_MSG;
     }
     if (this.f.password.hasError('forbiddenPassword')) {
-      return 'Password should not contain first name or last name';
+      return signupFormConstants.PASSWORD_FORBIDDEN_VALIDATION_MSG;
     }
-    return this.f.password.hasError('password') ? 'Not a valid password' : '';
   }
 }
