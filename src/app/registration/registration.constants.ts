6be4783faf3ea97e1f0837c68aea3ef5ea8388cc
gangadharjannu <gangadhar014@gmail.com>
2020-07-02 22:13:19 +0200

    Feature - Adds registration constants

--- src/app/registration/registration.constants.ts
+++ src/app/registration/registration.constants.ts
@@ -1,18 +1,11 @@
-const REQUIRED = 'required';
-
 export const REGISTRATION_FORM_TITLE = 'Please signup here to create an account';
 export const PASSWORD_MIN_LENGTH = 8;
-
-export const FIRST_NAME_LABEL = 'First Name';
-export const FIRST_NAME_VALIDATION_REQUIRED = `${FIRST_NAME_LABEL} ${REQUIRED}`;
-
-export const LAST_NAME_LABEL = 'Last Name';
-export const LAST_NAME_VALIDATION_REQUIRED = `${LAST_NAME_LABEL} ${REQUIRED}`;
-
-export const EMAIL_LABEL = 'Email';
-export const EMAIL_VALIDATION_REQUIRED = `${EMAIL_LABEL} ${REQUIRED}`;
-export const EMAIL_VALIDATION_INVALID = `${EMAIL_LABEL} must be a valid email`;
-
-export const PASSWORD_LABEL = 'Password';
-export const PASSWORD_VALIDATION_REQUIRED = `${PASSWORD_LABEL} ${REQUIRED}`;
-export const PASSWORD_VALIDATION_MIN_LENGTH = `${PASSWORD_LABEL} must be at least ${PASSWORD_MIN_LENGTH} characters`;
+export const REQUIRED_VALIDATION_MSG = 'You must enter a value';
+export const PASSWORD_MIN_LENGTH_VALIDATION_MSG = 'Password should be at least 8 characters length';
+export const PASSWORD_FORBIDDEN_VALIDATION_MSG = 'Password should not contain first name or last name';
+export const INVALID_EMAIL_MSG = 'Not a valid email';
+export const USER_REGISTRATION_SUCCESSFUL = 'Registration successful';
+export const USER_REGISTRATION_FAIL = 'Registration failed';
+export const USER_REGISTRATION_ALERT_CLOSE = 'Close';
+export const SUCCESSFUL_MSG_DURATION = 3000;
+export const FAILURE_MSG_DURATION = 5000;
