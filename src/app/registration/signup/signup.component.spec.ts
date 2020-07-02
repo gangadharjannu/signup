415fba23f557c751cd31fb0c37a3f8a3776f4e1b
gangadharjannu <gangadhar014@gmail.com>
2020-07-02 22:18:12 +0200

    Feature - Adds and updates test cases for signup component

--- src/app/registration/signup/signup.component.spec.ts
+++ src/app/registration/signup/signup.component.spec.ts
@@ -1,19 +1,20 @@
-import { RegistrationService } from './../registration.service';
-import { User } from './../../models/user';
-import { HttpClientModule } from '@angular/common/http';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { ReactiveFormsModule } from '@angular/forms';
-import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
-
-import { SignupComponent } from './signup.component';
-import { throwError, of } from 'rxjs';
 import { CommonModule } from '@angular/common';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { of } from 'rxjs'; // make sure to import the throwError from rxjs
+import { User } from './../../models/user';
+import { RegistrationService } from './../registration.service';
+import { SignupComponent } from './signup.component';
+import { PASSWORD_FORBIDDEN_VALIDATION_MSG, PASSWORD_MIN_LENGTH_VALIDATION_MSG } from '../registration.constants';
+
 
 describe('SignupComponent', () => {
   let component: SignupComponent;
@@ -21,16 +22,23 @@ describe('SignupComponent', () => {
   let signupEl;
   let registrationServiceSpy;
   const user: User = {
-    firstName: 'fn',
-    lastName: 'ln',
+    firstName: 'firstname',
+    lastName: 'lastname',
     email: 'em@i.l'
   };
-  const mockSignupFormFields: User = {
-    firstName: 'fn',
-    lastName: 'ln',
+  const validMockSignupFormFields: User = {
+    firstName: 'firstname',
+    lastName: 'lastname',
     email: 'em@i.l',
     password: 'passwordmin8char'
   };
+  const invalidMockSignupFormFields: User = {
+    firstName: '',
+    lastName: '',
+    email: 'emai.l',
+    password: 'passwd'
+  };
+  const invalidEmailMessage = 'Not a valid email';
 
   beforeEach(async(() => {
     const registrationService = jasmine.createSpyObj('RegistrationService', ['register']);
@@ -74,14 +82,14 @@ describe('SignupComponent', () => {
     expect(component.signupForm.valid).toBeFalsy();
   });
 
-  Object.keys(mockSignupFormFields).forEach(formControlKey => {
+  Object.keys(validMockSignupFormFields).forEach(formControlKey => {
     it(`${formControlKey} field validity`, () => {
       const formControl = component.signupForm.controls[formControlKey];
       expect(formControl.valid).toBeFalsy();
     });
   });
 
-  Object.keys(mockSignupFormFields).forEach(formControlKey => {
+  Object.keys(validMockSignupFormFields).forEach(formControlKey => {
     it(`${formControlKey} field validity required`, () => {
       const formControl = component.signupForm.controls[formControlKey];
       const { errors } = formControl || {};
@@ -89,81 +97,92 @@ describe('SignupComponent', () => {
     });
   });
 
-  it('form fields validity', () => {
-    Object.keys(mockSignupFormFields).forEach(formControlKey => {
+  it('check valid values for form fields', () => {
+    Object.keys(validMockSignupFormFields).forEach(formControlKey => {
       const formControl = component.signupForm.controls[formControlKey];
-      formControl.setValue(mockSignupFormFields[formControlKey]);
+      formControl.setValue(validMockSignupFormFields[formControlKey]);
       expect(formControl.valid).toBeTruthy();
+    });
+  });
+
+  it('check invalid values for form fields', () => {
+    Object.keys(validMockSignupFormFields).forEach(formControlKey => {
+      const formControl = component.signupForm.controls[formControlKey];
+      formControl.setValue(invalidMockSignupFormFields[formControlKey]);
+      expect(formControl.valid).toBeFalsy();
     });
   });
 
   it('email should be valid', () => {
     const emailFormControl = component.signupForm.controls.email;
-    emailFormControl.setValue('asdf');
+    emailFormControl.setValue(validMockSignupFormFields.email);
+    expect(emailFormControl.valid).toBeTruthy();
+    expect(component.getEmailErrorMessage()).toEqual('');
+  });
+
+  it('email should be invalid', () => {
+    const emailFormControl = component.signupForm.controls.email;
+    emailFormControl.setValue(invalidMockSignupFormFields.email);
     expect(emailFormControl.valid).toBeFalsy();
     expect(emailFormControl.errors.email).toBeTruthy();
+    expect(component.getEmailErrorMessage()).toEqual(invalidEmailMessage);
   });
 
   it('password should not contain firstname or lastname', () => {
-    component.signupForm.controls.firstName.setValue('first');
-    component.signupForm.controls.lastName.setValue('last');
     const passwordFormControl = component.signupForm.controls.password;
-    passwordFormControl.setValue('first');
+
+    component.signupForm.controls.firstName.setValue(validMockSignupFormFields.firstName);
+    component.signupForm.controls.lastName.setValue(validMockSignupFormFields.lastName);
+    passwordFormControl.setValue(validMockSignupFormFields.firstName);
+
     expect(passwordFormControl.valid).toBeFalsy();
     expect(passwordFormControl.errors.forbiddenPassword).toBeTruthy();
-    passwordFormControl.setValue('last');
-    expect(passwordFormControl.valid).toBeFalsy();
-    expect(passwordFormControl.errors.forbiddenPassword).toBeTruthy();
-    passwordFormControl.setValue('firstlast');
-    expect(passwordFormControl.valid).toBeFalsy();
-    expect(passwordFormControl.errors.forbiddenPassword).toBeTruthy();
-    passwordFormControl.setValue('lastfirst');
-    expect(passwordFormControl.valid).toBeFalsy();
-    expect(passwordFormControl.errors.forbiddenPassword).toBeTruthy();
-    passwordFormControl.setValue('min');
-    expect(passwordFormControl.valid).toBeFalsy();
-    expect(passwordFormControl.errors.minlength).toBeTruthy();
-    passwordFormControl.setValue('minpasswordchar');
+    expect(component.getPasswordErrorMessage()).toEqual(PASSWORD_FORBIDDEN_VALIDATION_MSG);
+
+    passwordFormControl.setValue(validMockSignupFormFields.password);
     expect(passwordFormControl.valid).toBeTruthy();
   });
 
+  it('should show min length error message if the password length is less than min length ', () => {
+    const passwordFormControl = component.signupForm.controls.password;
+    passwordFormControl.setValue(invalidMockSignupFormFields.password);
+    expect(passwordFormControl.valid).toBeFalsy();
+    expect(passwordFormControl.errors.minlength).toBeTruthy();
+    expect(component.getPasswordErrorMessage()).toEqual(PASSWORD_MIN_LENGTH_VALIDATION_MSG);
+  });
+
+  it('should not call register method of registration service on init', () => {
+    fixture.detectChanges();
+    expect(registrationServiceSpy.calls.any()).toBe(false, 'register called');
+  });
+
   it('should submit form', () => {
-    Object.keys(mockSignupFormFields).forEach(formControlKey => {
+    Object.keys(validMockSignupFormFields).forEach(formControlKey => {
       const formControl = component.signupForm.controls[formControlKey];
-      formControl.setValue(mockSignupFormFields[formControlKey]);
+      formControl.setValue(validMockSignupFormFields[formControlKey]);
       expect(formControl.valid).toBeTruthy();
     });
   });
 
-  it('should not call register method of registration service on init', () => {
-    fixture.detectChanges(); // onInit()
-    expect(registrationServiceSpy.calls.any()).toBe(false, 'register called');
+  it('submitting an invalid form stops creating user', () => {
+    expect(component.signupForm.valid).toBeFalsy();
+    Object.keys(validMockSignupFormFields).forEach(formControlKey => {
+      component.signupForm.controls[formControlKey].setValue(invalidMockSignupFormFields[formControlKey]);
+    });
+
+    expect(component.signupForm.valid).toBeFalsy();
+    component.submitSignupForm();
+    expect(registrationServiceSpy.calls.any()).toBe(false);
   });
+
   it('submitting a form emits a user', () => {
     expect(component.signupForm.valid).toBeFalsy();
-    Object.keys(mockSignupFormFields).forEach(formControlKey => {
-      component.signupForm.controls[formControlKey].setValue(mockSignupFormFields[formControlKey]);
+    Object.keys(validMockSignupFormFields).forEach(formControlKey => {
+      component.signupForm.controls[formControlKey].setValue(validMockSignupFormFields[formControlKey]);
     });
 
     expect(component.signupForm.valid).toBeTruthy();
-
     component.submitSignupForm();
-
     expect(registrationServiceSpy.calls.any()).toBe(true, 'register called');
   });
-  // it('should display error when TwainService fails', fakeAsync(() => {
-  //   // tell spy to return an error observable
-  //   getQuoteSpy.and.returnValue(
-  //     throwError('TwainService test failure'));
-
-  //   fixture.detectChanges(); // onInit()
-  //   // sync spy errors immediately after init
-
-  //   tick(); // flush the component's setTimeout()
-
-  //   fixture.detectChanges(); // update errorMessage within setTimeout()
-
-  //   expect(errorMessage()).toMatch(/test failure/, 'should display error');
-  //   expect(quoteEl.textContent).toBe('...', 'should show placeholder');
-  // }));
 });
